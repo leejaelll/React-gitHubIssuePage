859621c1a4c2adc8ebd78213cb248daa66515030
leejaelll <jayaom91076@gmail.com>
2023-02-01 18:15:39 +0900

    feat: Conditon Badge render
    - IssueBadge prop이 있을 때만 뱃지를 렌더링하도록 조건문 작성

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,14 @@
 import styles from '../styles/ListItem.module.css';
 import ListItemLayout from './ListItemLayout';
+import IssueBadge from './IssueBadge';
 
-export default function ListItem({ checked, onChangeCheckBox, onClickTitle }) {
+export default function ListItem({ checked, onChangeCheckBox, onClickTitle, IssueBadges }) {
   return (
     <ListItemLayout>
       <div onClick={onClickTitle} className={styles.title} role="button">
         Issue Expample
+        {/* IssueBadges가 있을때만 IssueBadges를 그릴 수 있도록 만든다 */}
+        {IssueBadges && IssueBadges.map((badgeProps, idx) => <IssueBadge key={idx} {...badgeProps} />)}
         <div className={styles.description}># Description</div>
       </div>
     </ListItemLayout>
